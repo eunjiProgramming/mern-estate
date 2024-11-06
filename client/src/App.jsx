e81601211a3a4c1aba10aaeab6150782b574a94f
eunjiProgramming <estelle.programming@gmail.com>
2024-11-06 15:22:57 +0900

    오타 정정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Signin from './pages/Signin';
+import SignIn from './pages/SignIn';
 import About from './pages/About';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
@@ -17,7 +17,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<Signin />} />
+              <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
         <Route path='/search' element={<Search />} />
